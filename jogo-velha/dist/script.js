40beaf78c360a61acdca1d86a7fb9d33a0de22eb
DIEGO MAGNO DE MORAES TOLEDO <diegommorais12@gmail.com>
2018-12-28 18:10:15 -0200

    finalizando jogo da velha

--- jogo-velha/dist/script.js
+++ jogo-velha/dist/script.js
@@ -1,171 +1,181 @@
 window.onload = () => {
-    new JogoVelha();
+  new JogoVelha();
 };
 
 class JogoVelha {
+  constructor() {
+    this.iniciaElementos();
+    this.iniciaEstado();
+  }
 
-    constructor() {
-        this.iniciaElementos();
-        this.iniciaEstado();
+  iniciaEstado() {
+    this.turno = true;
+    this.jogadas = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.fim = false;
+    this.vitoria = [448, 56, 7, 292, 146, 73, 273, 84];
+  }
+
+  iniciaElementos() {
+    this.jogadorX = document.querySelector("#jogador-x");
+    this.jogadorO = document.querySelector("#jogador-o");
+
+    this.salvarLocal = document.querySelector("#salva-local");
+    this.salvarLocal.addEventListener("click", this.salvaLocal.bind(this));
+
+    this.carregarLocal = document.querySelector("#carrega-local");
+    this.carregarLocal.addEventListener("click", this.carregaLocal.bind(this));
+
+    this.salvar = document.querySelector("#salvar");
+    this.salvar.addEventListener("click", this.enviarServidor.bind(this));
+
+    this.limparLocal = document.querySelector("#limpar");
+    this.limparLocal.addEventListener("click", this.limpaLocal.bind(this));
+
+    this.velha = document.querySelector("#velha");
+    this.velha.addEventListener("click", event => {
+      this.realizaJogada(event);
+      this.render();
+    });
+  }
+
+  salvaLocal() {
+    const dados = {
+      jogadorX: this.jogadorX.value,
+      jogadorO: this.jogadorO.value,
+      jogadas: this.jogadas
+    };
+
+    localStorage.setItem("jogo", JSON.stringify(dados));
+  }
+
+  carregaLocal() {
+    const dados = JSON.parse(localStorage.getItem("jogo"));
+    this.jogadorO.value = dados.jogadorO;
+    this.jogadorX.value = dados.jogadorX;
+    this.jogadas = dados.jogadas;
+
+    this.render();
+  }
+
+  limpaLocal() {
+    localStorage.removeItem("jogo");
+    this.jogadorO.value = "";
+    this.jogadorX.value = "";
+    this.iniciaEstado();
+
+    this.render();
+  }
+
+  realizaJogada(event) {
+    const id = event.target.dataset.id;
+
+    if (this.fim) {
+      this.modal("Partida terminada");
+      return;
     }
 
-    iniciaEstado() {
-        this.turno = true;
-        this.jogadas = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        this.fim = false;
-        this.vitoria = [448, 56, 7, 292, 146, 73, 273, 84];
+    // verifica se escolheu uma casa correta
+    if (!event.target.dataset.id) {
+     this.modal("Você precisa clicar em uma casa");
+      return;
     }
 
-    iniciaElementos() {
-
-        this.jogadorX = document.querySelector("#jogadorX");
-        this.jogadorO = document.querySelector("#jogadorO");
-
-
-        this.salvarLocal = document.querySelector("#salvar");
-        this.salvarLocal.addEventListener('click', this.salvaLocal.bind(this));
-
-        this.carregarLocal = document.querySelector("#carregar");
-        this.carregarLocal.addEventListener('click', this.carregaLocal.bind(this));
-
-        this.limparLocal = document.querySelector("#limpar");
-        this.limparLocal.addEventListener('click', this.limpaLocal.bind(this));
-
-        this.salvarImg = document.querySelector("#salvarImg");
-        this.salvarImg.addEventListener('click', this.enviaServidor.bind(this));
-
-        this.velha = document.querySelector('#velha');
-        this.velha.addEventListener('click', (event) => {
-            this.realizaJogada(event);
-            this.render();
-        });
+     // verifica se a posição é válida
+     if (this.jogadas[id] != 0) {
+      this.modal("Esta posição já foi escolhida");
+      return;
     }
 
-    salvaLocal() {
+    this.jogadas[id] = this.turno ? "X" : "O";
 
+    this.turno = !this.turno;
+  }
 
-        const dados = {
-            jogadorX: this.jogadorX.value,
-            jogadorO: this.jogadorO.value,
-            jogadas: this.jogadas,
-        }
+  render() {
+    const resultado = this.verificaVitoria();
 
-        localStorage.setItem('jogo', JSON.stringify(dados));
-    }
+    if (resultado == "X" || resultado == "O") {
+      this.fim = true;
+      this.salvar.style.display = "block";
 
-    carregaLocal() {
-        const dados = JSON.parse(localStorage.getItem('jogo'));
-        this.jogadorO.value = dados.jogadorO;
-        this.jogadorX.value = dados.jogadorX;
-        this.jogadas = dados.jogadas;
-
-        this.render();
-
-    }
-    limpaLocal() {
-        localStorage.removeItem('jogo');
-        this.jogadorO.value = '';
-        this.jogadorX.value = '';
-        this.iniciaEstado();
-        this.render
-    }
-
-    realizaJogada(event) {
-        const id = event.target.dataset.id;
-        if (this.fim) {
-            this.modal("partida encerrada");
-            return;
-        }
-        //verifica se escolheu uma casa valida
-        if (!event.target.dataset.id) {
-            this.modal("clique em uma casa válida");
-            return;
-        }
-        //verifica se a posição já foi escolhida
-        if (this.jogadas[id] != 0) {
-            this.modal('essa opção já foi escolhida');
-            return;
-        }
-        this.jogadas[id] = this.turno ? 'X' : 'O';
-
-        this.turno = !this.turno;
-    }
-    modal(mensagem) {
-        const modais = document.querySelector('#modal');
-        const modal = document.createElement('div');
-        modal.innerHTML = mensagem;
-        modal.classList.add("modalClass");
-        modais.appendChild(modal);
-
-        setTimeout(() => {
-            modal.classList.add('remover');
-            setTimeout(() => {
-                modais.removeChild(modal);
-            }, 1500)
-        }, 2000)
+      this.modal(`O jogador ${resultado} venceu!`);
+    }else{
+      this.salvar.style.display = "none";
 
     }
 
-    render() {
-        const resultado = this.verificaVitoria();
-        if (resultado == 'X' || resultado == 'O') {
-            this.fim = true;
-            this.salvarImg.style.display = "block";
-            this.modal(`O jogador ${resultado} venceu!`);
-
-        } else
-            this.salvarImg.style.display = "none";
-
-
-        //iterar nos elementos do tabuleiro
-        const velhaElemento = document.querySelectorAll('[data-id]');
-        for (let i = 0; i < 9; i++) {
-            velhaElemento[i].innerHTML = this.jogadas[i] == 0 ? "" : this.jogadas[i];
-
-        }
-
-
+    //iterar nos elementos do tabuleiro
+    const velhaElemento = document.querySelectorAll("[data-id]");
+    for (let i = 0; i < 9; i++) {
+      velhaElemento[i].innerHTML = this.jogadas[i] == 0 ? "" : this.jogadas[i];
     }
-    verificaVitoria() {
-        const valorX = parseInt(this.jogadas.map(value => value == 'X' ? 1 : 0).join(''), 2);
-        const valorO = parseInt(this.jogadas.map(value => value == 'O' ? 1 : 0).join(''), 2);
-        console.log(valorX);
-        for (const element of this.vitoria) {
-            if ((element & valorX) == element) {
-                return 'X';
-            }
-            if ((element & valorO) == element) {
-                return 'O';
-            }
-        }
-        return "";
+  }
 
+  verificaVitoria() {
+    //decimal da sequencia de quem jogou x
+    const valorX = parseInt(
+      this.jogadas.map(value => (value == "X" ? 1 : 0)).join(""),
+      2
+    );
+    //decimal da sequencia de quem jogou y
+    const valorO = parseInt(
+      this.jogadas.map(value => (value == "O" ? 1 : 0)).join(""),
+      2
+    );
 
-
+    //percorrer array vitoria perguntando
+    for (const element of this.vitoria) {
+      if ((element & valorX) == element) {
+        return "X";
+      }
+      if ((element & valorO) == element) {
+        return "O";
+      }
     }
-    enviaServidor() {
-        // Receber nome dos jogadores
-        const jogadorX = this.jogadorX.value;
-        const jogadorO = this.jogadorO.value;
 
-        //criar imagem do estado atual do  tabuleiro
-        domtoimage.toPng(this.velha, { width: '400', height: '400' })
-            .then((dataUrl) => {
+    return "";
+  }
 
+  modal(texto){
+    const modais = document.querySelector("#modais");
+    const modal = document.createElement("div");
+    modal.innerHTML = texto;
+    modal.classList.add("modalClass");
+    modais.appendChild(modal);
 
-                return axios.post('/save', {
-                    jogadorX, jogadorO, jogadas: JSON.stringify(this.jogadas), img: dataUrl
-                })
+    setTimeout(() => {
+      modal.classList.add("remover");
+      
+      setTimeout(() => {
+        modais.removeChild(modal);
+  
+      }, 1000);
 
-            })
-            .then(function (response) {
-                this.modal('Emviado com sucesso!')
-            })
+    }, 2000);
 
-            .catch((error) => {
-                this.modal('oops, something went wrong!', error);
-            })
+  }
 
-        //repassar a imagem criada para o servidor
-    }
+  enviarServidor(){
+    //receber os nomes dos usuarios
+    const jogadorX = this.jogadorX.value;
+    const jogadorO = this.jogadorO.value;
+
+    //criar a imagem do tabuleiro
+    domtoimage.toPng(this.velha, {width: '400', height: '400'})
+    .then((dataUrl) =>{
+       
+        return axios.post('/save', {
+          jogadorX, jogadorO, jogadas : JSON.stringify(this.jogadas),
+          img: dataUrl
+        })
+       
+    }).then((response) => {
+      this.modal('Envio com sucesso')
+    })
+    .catch((error) =>{
+        this.modal('oops, something went wrong!', error);
+    });
+      //repassa para o servidor os dados
+
+  }
 }
